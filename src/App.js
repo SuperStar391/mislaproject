bae7b67fffe733c4479b3e2f83ec7f939fc64ad5
superstar <wxin0721@gmail.com>
2021-11-24 05:39:08 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { useState, useReducer } from 'react'
+import './App.css';
+
+import CharacterMap from './components/CharacterMap';
+
+function App() {
+  const [text, setText] = useState('');
+  const [showExplanation, toggleExplanation] = useReducer(state => !state, false)
+
+  return (
+    <div className="wrapper">
+      <label htmlFor="text">
+        <p>Add Your Text Here:</p>
+        <textarea
+          id="text"
+          name="text"
+          rows="10"
+          cols="100"
+          onChange={event => setText(event.target.value)}
+        >
+        </textarea>
+      </label>
+      <div>
+        <button onClick={toggleExplanation}>Show Explanation</button>
+      </div>
+      {showExplanation &&
+        <p>
+          This displays a list of the most common characters.
+        </p>
+      }
+      <CharacterMap text={text} />
+    </div>
+  );
+}
+
+export default App;
